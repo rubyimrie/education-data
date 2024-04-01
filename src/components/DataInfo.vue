a40d30cfefdb2f0e1c50b01b22de5fc0c85381f1
Ruby Imrie <s2056921@ed.ac.uk>
2024-04-01 17:46:09 +0100

    adding publicly available tooltip

--- src/components/DataInfo.vue
+++ src/components/DataInfo.vue
@@ -4,7 +4,21 @@
     <p class="mb-4 ml-1"><span class="font-semibold">Summary:</span> {{ data.content }}</p>
     <p class="mb-4 ml-1"><span class="font-semibold">Format Available:</span> <span class="bg-cutty-sark-600 text-white rounded px-2">{{ data.format }}</span></p>
     <p class="mb-4 ml-1"><span class="font-semibold">Link:</span> <a :href="data.link" target="_blank" class="text-blue-500 hover:underline">{{ data.link }}</a></p>
-    <p class="flex items-center mb-2 ml-1"><span class="font-semibold">Publicly Available: </span><span><svg v-if="data.available==='TRUE'" class="tick-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill="#4CAF50" d="M18.585 3.414a2 2 0 0 0-2.828 0L8 11.172 4.243 7.415a2 2 0 0 0-2.828 2.828l4.949 4.95a2 2 0 0 0 2.828 0L18.585 6.243a2 2 0 0 0 0-2.829z"/></svg><svg v-else class="cross-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill="#F44336" d="M10 1a9 9 0 1 0 0 18 9 9 0 0 0 0-18zm4.95 12.121l-1.414 1.415L10 11.414l-3.536 3.536-1.414-1.415L8.586 10l-3.535-3.536 1.414-1.415L10 8.586l3.536-3.535 1.414 1.414L11.414 10l3.536 3.536z"/></svg></span><p v-if="data.available !== 'TRUE'" class="text-sm ml-2">  This data is available by request only</p></p>
+    <div @mouseover="showAvailableInfo = true" @mouseleave="showAvailableInfo = false">
+      <div class="inline-flex flex justify-center items-center relative mb-2">
+        <h3 class=" font-semibold flex relative">
+          <span class="font-semibold inline-flex">Publicly Available: <img width="20" height="20" src="https://img.icons8.com/windows/32/info-squared.png" alt="info-squared"/>: </span>
+        </h3>
+        <span >
+          <svg v-if="data.available==='TRUE'" class="tick-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill="#4CAF50" d="M18.585 3.414a2 2 0 0 0-2.828 0L8 11.172 4.243 7.415a2 2 0 0 0-2.828 2.828l4.949 4.95a2 2 0 0 0 2.828 0L18.585 6.243a2 2 0 0 0 0-2.829z"/></svg>
+          <svg v-else class="cross-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill="#F44336" d="M10 1a9 9 0 1 0 0 18 9 9 0 0 0 0-18zm4.95 12.121l-1.414 1.415L10 11.414l-3.536 3.536-1.414-1.415L8.586 10l-3.535-3.536 1.414-1.415L10 8.586l3.536-3.535 1.414 1.414L11.414 10l3.536 3.536z"/></svg>
+        </span>
+        <span v-if="data.available !== 'TRUE'" class="text-sm ml-2">  This data is available by request only</span>
+      </div>
+    <div v-if="showAvailableInfo" class="absolute bg-white border border-gray-300 p-2 rounded-lg shadow-md" style="width: 30vw;">
+              {{ availableInfo }}
+          </div>
+    </div>
     <p class="mb-4 ml-1"><span class="font-semibold">Field Names: </span>{{ data.fields }}</p>
     <!-- Display tags as buttons -->
     <div class="flex flex-wrap">
@@ -40,6 +54,11 @@ export default {
       required: true
     }
   },
+  data() {
+      return {
+        showAvailableInfo: false,
+        availableInfo: 'To find or request the data follow the link above.'
+      }},
   methods: {
     // Emit event when a tag is clicked
     handleTagClick(tag) {
